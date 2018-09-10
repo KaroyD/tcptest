df47fe1486f1c2ab6178e9cd8ee7b0b28bee9bcf
Karoy <641707674@qq.com>
2018-09-10 11:40:54 +0800

    日志类

---
+++ src/cn/scjfl/log/Log.java
@@ -0,0 +1,16 @@
+package cn.scjfl.log;
+
+import org.apache.log4j.Logger;
+
+/**
+*
+*Description:
+*@author Karoy
+*Start time:2018年9月6日
+*Version:v0.1 2018年9月6日
+*
+*/
+
+public class Log {
+	public static Logger log = Logger.getLogger(Log.class.getName());
+}
